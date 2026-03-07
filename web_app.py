2d0d92367ed750dd94814d9bac7f3c22a2803528
avsn17 <avasingueneser1@gmail.com>
2026-03-07 01:21:02 +0000

    add Railway deployment config

--- web_app.py
+++ web_app.py
@@ -170,4 +170,4 @@ def inhale(i):
         save_data(d)
     return jsonify(ok=True)
 
-if __name__ == '__main__': app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__': app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
